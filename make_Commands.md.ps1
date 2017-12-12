5a43bc83349aa92866185cabe6554f801c11a9cf
Robert Jäckel <robert.jaeckel@itz.uni-halle.de>
2017-12-12 14:13:54 +0100

    placed outputfile in Script

--- make_Commands.md.ps1
+++ make_Commands.md.ps1
@@ -1,39 +1,40 @@
 Import-Module ./bConnect.psd1 -Force
-
-"# Cmdlets exported by this module"
-""
-"## Initialize"
-'```'
-(Get-Help Initialize-bConnect).Synopsis.trim()
-'```'
-""
-"## Commands by Controller"
-( Get-Command -Module bConnect |? {$_ -notmatch '^(New|Merge|Expand|Search|Initialize)-'}|
-  Sort-Object Noun,Verb|Group-Object Noun
-) | % {
+& {
+    "# Cmdlets exported by this module"
     ""
-    "### Controller "+($_.Name -replace '^b')
-    $_.Group.Name |% {
+    "## Initialize"
+    '```'
+    (Get-Help Initialize-bConnect).Synopsis.trim()
+    '```'
+    ""
+    "## Commands by Controller"
+    ( Get-Command -Module bConnect |? {$_ -notmatch '^(New|Merge|Expand|Search|Initialize)-'}|
+    Sort-Object Noun,Verb|Group-Object Noun
+    ) | % {
+        ""
+        "### Controller "+($_.Name -replace '^b')
+        $_.Group.Name |% {
+            "#### $_"
+            '```'
+            (Get-Help $_).Synopsis.trim()
+            '```'
+        }
+    }
+    ""
+    "### Controller Search"
+    (Get-Command -Module bConnect -Verb Search).Name |% {
         "#### $_"
         '```'
         (Get-Help $_).Synopsis.trim()
         '```'
     }
-}
-""
-"### Controller Search"
-(Get-Command -Module bConnect -Verb Search).Name |% {
-    "#### $_"
-    '```'
-    (Get-Help $_).Synopsis.trim()
-    '```'
-}
-""
-"## Helper Commands"
-""
-(Get-Command -Module bConnect |? {$_ -match '^(New|Merge|Expand)-'} | Sort-Object Verb,Noun).Name|% {
-    "### $_"
-    '```'
-    (Get-Help $_).Synopsis.trim()
-    '```'
-}
+    ""
+    "## Helper Commands"
+    ""
+    (Get-Command -Module bConnect |? {$_ -match '^(New|Merge|Expand)-'} | Sort-Object Verb,Noun).Name|% {
+        "### $_"
+        '```'
+        (Get-Help $_).Synopsis.trim()
+        '```'
+    }
+} > ./Commands.md
